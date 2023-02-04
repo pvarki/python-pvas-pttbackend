63c25c040e8114a0070a222077351226c803e69a
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-02-04 14:15:45 +0200

    helper for quick and dirty database creation, fetch the bearer token we need for calling pipelines from KeyVault

--- src/pttbackend/security.py
+++ src/pttbackend/security.py
@@ -1,11 +1,19 @@
 """Security stuff"""
 from typing import Optional, Any
 import logging
+from dataclasses import dataclass, field
 
+
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
 from libadvian.binpackers import ensure_utf8
 from arkia11napi.security import JWTBearer, JWTPayload, check_acl, JWTHandler as _JWTHandler
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
+
+
+from .config import PIPELINE_TOKEN_KEYVAULT, PIPELINE_TOKEN_SECRETNAME
+
 
 LOGGER = logging.getLogger(__name__)
 
@@ -37,6 +45,36 @@ class JWTHandler(_JWTHandler):
 
 
 HDL_SINGLETON: Optional[JWTHandler] = None
+
+
+@dataclass
+class PipelineTokens:
+    """Wrap the secret fetch to a singleton pattern"""
+
+    bearer: Optional[str] = field(default=None, repr=False)
+
+    kvuri: str = field(default=f"https://{PIPELINE_TOKEN_KEYVAULT}.vault.azure.net")
+    _credentials: DefaultAzureCredential = field(default_factory=DefaultAzureCredential)
+    _client: SecretClient = field(init=False, repr=False)
+
+    def __post_init__(self) -> None:
+        """Fetch the keys here"""
+        self._client = SecretClient(vault_url=self.kvuri, credential=self._credentials)
+        secret = self._client.get_secret(PIPELINE_TOKEN_SECRETNAME)
+        self.bearer = secret.value
+
+    @classmethod
+    def singleton(cls, **kwargs: Any) -> "PipelineTokens":
+        """Get a singleton"""
+        global KVTOKEN_SINGLETON  # pylint: disable=W0603
+        if KVTOKEN_SINGLETON is None:
+            KVTOKEN_SINGLETON = PipelineTokens(**kwargs)
+        assert KVTOKEN_SINGLETON is not None
+        return KVTOKEN_SINGLETON
+
+
+KVTOKEN_SINGLETON: Optional[PipelineTokens] = None
+
 
 # re-export some helpers
 __all__ = ["JWTHandler", "JWTBearer", "JWTPayload", "check_acl"]
