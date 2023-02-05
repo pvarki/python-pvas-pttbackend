41f56eee081608dc051c3d7958b7af862c55afd5
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-02-05 14:06:38 +0200

    add quick and dirty instructions html

--- src/pttbackend/api.py
+++ src/pttbackend/api.py
@@ -4,20 +4,17 @@ import logging
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 from libadvian.logging import init_logging
 from arkia11napi.middleware import DBWrapper
 
-from .config import TEMPLATES_PATH, STATIC_PATH, LOG_LEVEL
+from .config import STATIC_PATH, LOG_LEVEL
 from .views.instances import INSTANCE_ROUTER
 from .views.callbacks import CALLBACKS_ROUTER
 
 from . import models
 
 
-TEMPLATES = Jinja2Templates(directory=str(TEMPLATES_PATH))
 LOGGER = logging.getLogger(__name__)
-
 APP = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
 APP.mount("/static", StaticFiles(directory=str(STATIC_PATH)), name="static")
 APP.include_router(CALLBACKS_ROUTER)
--- src/pttbackend/views/instances.py
+++ src/pttbackend/views/instances.py
@@ -4,18 +4,23 @@ import logging
 import uuid
 
 import pendulum
-from fastapi import APIRouter, Depends, Request, HTTPException
+from fastapi import APIRouter, Depends, Request, HTTPException, Response
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from starlette import status
 from arkia11napi.helpers import get_or_404
 from arkia11napi.security import JWTBearer, check_acl
 
 
+from ..config import TEMPLATES_PATH
 from ..schemas.instance import DBInstance, InstanceCreate, InstancePager
 from ..models import PTTInstance
 from ..pipelineclient import PipeLineClient
+from .. import config
 
 
 LOGGER = logging.getLogger(__name__)
+TEMPLATES = Jinja2Templates(directory=str(TEMPLATES_PATH))
 INSTANCE_ROUTER = APIRouter(dependencies=[Depends(JWTBearer(auto_error=True))])
 
 
@@ -105,3 +110,24 @@ async def delete_instance(request: Request, pkstr: str) -> None:
         LOGGER.exception("Could not trigger pipeline {}".format(exc))
         raise
     await instance.update(deleted=pendulum.now("UTC")).apply()
+
+
+@INSTANCE_ROUTER.get("/api/v1/ptt/instances/{pkstr}/instructions", tags=["ptt-instances"], response_class=HTMLResponse)
+async def get_instructions(request: Request, pkstr: str) -> Response:
+    """Show instructions HTML with server address etc"""
+    instance = await get_or_404(PTTInstance, pkstr)
+    if not check_acl(request.state.jwt, "fi.pvarki.pttbackend.instance:read", auto_error=False):
+        if instance.ownerid != request.state.jwt["userid"]:
+            raise HTTPException(status_code=403, detail="Required privilege not granted.")
+    return TEMPLATES.TemplateResponse(
+        "instructions.html",
+        {
+            "request": request,
+            "instructions_pdf": config.INSTRUCTIONS_URL,
+            "taisteluajatus_pdf": config.TAKORTTI_URL,
+            "templates_zip": config.DOCTEMPLATE_URL,
+            "dns_name": instance.tfoutputs["dns_name"],
+            "mumble_server_password": instance.tfoutputs["mumble_server_password"],
+            "mumble_superuser_password": instance.tfoutputs["mumble_superuser_password"],
+        },
+    )
