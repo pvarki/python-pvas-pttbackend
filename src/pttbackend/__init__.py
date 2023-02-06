dad0892ac0e626b2c7c766296d556304f6fc3936
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-02-06 20:37:54 +0200

    bump our version

--- src/pttbackend/__init__.py
+++ src/pttbackend/__init__.py
@@ -1,2 +1,2 @@
 """ Keeps track of mumble instances ordered by people in different contexts """
-__version__ = "0.1.0"  # NOTE Use `bump2version --config-file patch` to bump versions correctly
+__version__ = "1.0.0"  # NOTE Use `bump2version --config-file patch` to bump versions correctly
